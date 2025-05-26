f0eebe8fb062bd49057a81a94c37a1218060f118
Ontonieja <ontonieja@gmail.com>
2025-05-26 16:59:09 +0200

    create error handling middleware

---
+++ server/src/middlewares/errorHandler.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import AppError from '../utils/appError';
+import axios from 'axios';
+
+function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+  console.error(err);
+
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  return res.status(500).json({ message: 'An unexpected error ocurred', error: err.message });
+}
+
+export default errorHandler as (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => void;
