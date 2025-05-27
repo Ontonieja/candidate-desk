9185adc635e562092613a264a17e39cbbe161ac0
Ontonieja <ontonieja@gmail.com>
2025-05-27 11:32:19 +0200

    create service for single page fetching and filtering results

--- server/src/services/candidatesServices.ts
+++ server/src/services/candidatesServices.ts
@@ -1,16 +1,41 @@
-import { GetAllCandidatesDataOptions } from '../types/candidateTypes';
-import fetchAllPages from '../utils/fetchAllPages';
-import fetchTeamtailorApi from '../utils/fetchTeamtailorApi';
+import {
+  CandidatesData,
+  FetchPaginatedCandidatesReturn,
+  FilteredCandidate
+} from '@/types/candidateTypes';
+import fetchAllPages from '@/utils/fetchAllPages';
+import fetchTeamtailorApi from '@/utils/fetchTeamtailorApi';
+import filterCandidates from '@/utils/filterCandidates';
+import { GET_ALL_CANDIDATES_URL } from '@/routes/endpoints';
 
-export async function getAllCandidatesData() {
-  const url = 'https://api.teamtailor.com/v1/candidates?include=job-applications&page[size]=30';
+export async function getAllCandidatesData(): Promise<FilteredCandidate[]> {
+  const candidates = await fetchAllPages({ url: GET_ALL_CANDIDATES_URL });
 
-  const candidates = await fetchAllPages({ url });
+  return filterCandidates(candidates);
 }
 
-export async function fetchPaginatedCandidates(url: string) {
+export async function fetchPaginatedCandidates(
+  url: string
+): Promise<FetchPaginatedCandidatesReturn> {
   try {
-    return await fetchTeamtailorApi(url);
+    const response = await fetchTeamtailorApi(url);
+
+    const { data: candidatesData, included: applicationsData } = response;
+    const pageCount = response.meta?.['page-count'] ?? 0;
+    const nextPage = response.links?.next;
+
+    const filteredCandidates = filterCandidates({
+      candidates: candidatesData,
+      applications: applicationsData ?? []
+    });
+
+    return {
+      candidates: filteredCandidates,
+      meta: {
+        pageCount,
+        nextPage
+      }
+    };
   } catch (err) {
     throw err;
   }
