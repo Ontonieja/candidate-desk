e5217c99759daaf88bda25b9a025541885a4dd0d
Ontonieja <ontonieja@gmail.com>
2025-05-27 12:04:15 +0200

    code clean up

--- server/src/utils/fetchAllPages.ts
+++ server/src/utils/fetchAllPages.ts
@@ -23,9 +23,7 @@ export default async function fetchAllPages(
       applications: [...accumulatedData.applications, ...(currentApplications || [])]
     };
 
-    if (nextPage) {
-      return fetchAllPages({ url: nextPage, accumulatedData: updatedData });
-    }
+    if (nextPage) return fetchAllPages({ url: nextPage, accumulatedData: updatedData });
 
     return updatedData;
   } catch (err) {
