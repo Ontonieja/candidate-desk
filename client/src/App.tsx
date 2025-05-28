2f2254e63aaec3e4a5b5be682a43afbceb94f8e7
Ontonieja <ontonieja@gmail.com>
2025-05-28 08:02:33 +0200

    create app structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
+import Navigation from './components/nav/Nav';
+import BlurredShape from './components/ui/BlurredShape';
+
+import DataTableContainer from './components/table/DataTableContainer';
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='min-h-screen w-full relative overflow-hidden font-inter flex flex-col items-center py-8 px-8 sm:pt-10 sm:px-12 text-sm'>
+      <BlurredShape className='-top-50 -right-30 h-[400px] w-[240px] md:w-[320px] md:h-[300px] xl:w-[360px] 2xl:w-[500px]  bg-[#fef3c5] opacity-30'></BlurredShape>
+      <BlurredShape className='-bottom-80 -left-24 h-[400px] w-[240px] md:w-[320px] md:h-[400px] xl:w-[360px] 2xl:w-[600px]  bg-[#ffc7dc] opacity-50'></BlurredShape>
+
+      <Navigation />
+      <DataTableContainer />
     </div>
   );
 }
