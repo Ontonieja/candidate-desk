301d7d42a4fd7740c4d002239a1e8f7d735af369
Ontonieja <ontonieja@gmail.com>
2025-05-28 13:19:31 +0200

    styling changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,13 @@ import DataTableContainer from './components/table/DataTableContainer';
 
 export default function App() {
   return (
-    <div className='min-h-screen w-full relative overflow-hidden font-inter flex flex-col items-center py-8 px-8 sm:pt-10 sm:px-12 text-sm'>
+    <div className='min-h-screen w-full relative overflow-hidden font-inter flex flex-col items-center py-8 px-4 sm:px-8 lg:px-12 sm:pt-10 text-sm'>
       <BlurredShape className='-top-50 -right-30 h-[400px] w-[240px] md:w-[320px] md:h-[300px] xl:w-[360px] 2xl:w-[500px]  bg-[#fef3c5] opacity-30'></BlurredShape>
       <BlurredShape className='-bottom-80 -left-24 h-[400px] w-[240px] md:w-[320px] md:h-[400px] xl:w-[360px] 2xl:w-[600px]  bg-[#ffc7dc] opacity-50'></BlurredShape>
-
-      <Navigation />
-      <DataTableContainer />
+      <div className='w-full max-w-7xl'>
+        <Navigation />
+        <DataTableContainer />
+      </div>
     </div>
   );
 }
