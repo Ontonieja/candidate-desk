389b67a424c3111a77c6d0920bd12ee290910179
Ontonieja <ontonieja@gmail.com>
2025-05-28 13:16:58 +0200

    create searching functionality and refactor code

--- client/src/components/table/DataTableContainer.tsx
+++ client/src/components/table/DataTableContainer.tsx
@@ -1,7 +1,7 @@
 import { IoIosSearch } from 'react-icons/io';
 import DataTable from '../table/DataTable';
 import { FaFileExport } from 'react-icons/fa6';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { FaArrowRight, FaArrowLeft } from 'react-icons/fa6';
 import columns from './columns';
@@ -12,29 +12,50 @@ export default function DataTableContainer() {
   const [pageSize, setPageSize] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
   const [nextUrl, setNextUrl] = useState<string | null>(null);
+  const [search, setSearch] = useState<string>('');
 
   const queryOptions = useCandidatesQueryOptions(nextUrl, pageSize);
   const { isLoading, data, isFetching } = useQuery(queryOptions);
+  const { prevPage, recordCount = 0, nextPage, pageCount = 0 } = data?.meta ?? {};
+  const candidates = data?.candidates;
+
+  const currentCandidates =
+    candidates && candidates.length > pageSize ? candidates.slice(0, pageSize) : candidates;
 
   const handleNext = () => {
-    if (data?.meta?.nextPage) {
-      setNextUrl(data.meta.nextPage);
+    if (nextPage) {
+      setNextUrl(nextPage);
       setCurrentPage((prev) => prev + 1);
     }
   };
 
   const handlePrev = () => {
-    if (data?.meta?.prevPage) {
-      setNextUrl(data.meta.prevPage);
+    if (prevPage) {
+      setNextUrl(prevPage);
       setCurrentPage((prev) => prev - 1);
     }
   };
 
-  const currentCandidates = data?.candidates ? data.candidates.slice(0, pageSize) : [];
+  const filteredCandidates = useMemo(() => {
+    if (!search.trim()) return currentCandidates;
+
+    return currentCandidates?.filter((candidate: Candidate) => {
+      const searchLower = search.toLowerCase();
+      return [
+        candidate.first_name,
+        candidate.last_name,
+        candidate.email,
+        candidate.candidate_id?.toString(),
+        candidate.job_application_id?.toString()
+      ]
+        .filter(Boolean)
+        .some((field) => field.toLowerCase().includes(searchLower));
+    });
+  }, [currentCandidates, search]);
 
   return (
-    <main className='mt-8 md:mt-12 '>
-      <div className='flex justify-between'>
+    <div className='mt-8 md:mt-12 '>
+      <nav className='flex justify-between'>
         <h2 className='text-2xl font-bold hidden md:block'>Candidates</h2>
         <div className='gap-3 flex items-center w-full max-md:justify-between justify-end'>
           <div className='relative'>
@@ -42,6 +63,7 @@ export default function DataTableContainer() {
               type='text'
               className='border border-[#f0f0f0] text-xs sm:text-sm  w-full px-8 focus:outline-none rounded-xl py-2 sm:w-full'
               placeholder='Search candidates'
+              onChange={(e) => setSearch(e.target.value)}
             ></input>
             <IoIosSearch className='absolute top-1/2 left-3 -translate-y-1/2 size-4 text-gray-400' />
           </div>
@@ -57,19 +79,20 @@ export default function DataTableContainer() {
             </button>
           </div>
         </div>
-      </div>
-      <div className='w-full rounded-3xl text-xs bg-[#fbfbfb] mt-4 p-2'>
+      </nav>
+
+      <main className='w-full rounded-3xl text-xs bg-[#fbfbfb] mt-4 p-3'>
         <div className='w-full'>
           <DataTable
             columns={columns}
-            data={currentCandidates as Candidate[]}
+            data={filteredCandidates as Candidate[]}
             loading={isLoading}
             pageSize={pageSize}
           />
         </div>
-      </div>
+      </main>
 
-      <div className='flex justify-between text-xs md:px-4 mt-4 text-gray-700 font-medium'>
+      <div className='flex justify-between text-xs mt-4 text-gray-700 font-medium'>
         <div className='flex  items-center'>
           <select
             value={pageSize}
@@ -78,7 +101,7 @@ export default function DataTableContainer() {
               setNextUrl(null);
               setCurrentPage(1);
             }}
-            className='border border-[#f0f0f0] rounded-xl py-2 px-3 mr-2 focus:outline-none appearance-none'
+            className='border border-gray-200 rounded-xl py-2 px-3 mr-2 focus:outline-none appearance-none'
           >
             {[5, 10, 20, 30].map((size) => (
               <option className='outline-none' key={size} value={size}>
@@ -89,31 +112,33 @@ export default function DataTableContainer() {
 
           <p>
             {data?.meta
-              ? `${pageSize * (currentPage - 1) + 1} - ${Math.min(pageSize * currentPage, data.meta.recordCount)} of ${data.meta.recordCount}`
+              ? `${pageSize * (currentPage - 1) + 1} - ${Math.min(pageSize * currentPage, recordCount ?? 0)} of ${recordCount ?? 0}`
               : '...'}
           </p>
         </div>
 
         <div className='flex gap-4 items-center'>
-          {data?.meta?.prevPage && !isFetching && (
+          {prevPage && !isFetching && (
             <button
               onClick={handlePrev}
-              disabled={!data?.meta?.prevPage || isFetching}
+              disabled={!prevPage || isFetching}
               className='cursor-pointer hover:text-primary-accent ease-in-out duration-300 bg-transparent border-none'
             >
               <FaArrowLeft className='size-4' />
             </button>
           )}
-          <span>{data?.meta ? `${currentPage} of ${data.meta.pageCount}` : '...'}</span>
-          <button
-            onClick={handleNext}
-            disabled={!data?.meta?.nextPage || isFetching}
-            className='cursor-pointer hover:text-primary-accent ease-in-out duration-300 bg-transparent border-none'
-          >
-            <FaArrowRight className='size-4' />
-          </button>
+          <span>{pageCount && currentPage ? `${currentPage} of ${pageCount}` : '...'}</span>
+          {currentPage < pageCount && (
+            <button
+              onClick={handleNext}
+              disabled={!nextPage || isFetching}
+              className='cursor-pointer hover:text-primary-accent ease-in-out duration-300 bg-transparent border-none'
+            >
+              <FaArrowRight className='size-4' />
+            </button>
+          )}
         </div>
       </div>
-    </main>
+    </div>
   );
 }
